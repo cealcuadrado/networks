b30cc4a8e60cea18a6630c127ae884384c391ebf
CeAlCuadrado <cealcuadrado@outlook.com>
2020-06-30 11:25:29 -0400

    Auto-generated commit

---
+++ assets/data/networks.ts
@@ -0,0 +1,19 @@
+import { Network } from './../../app/shared/interfaces/network';
+
+export const NETWORKS: Network[] = [
+    {
+        title: 'Misceláneo al Cuadrado',
+        url: 'https://cealcuadrado.tumblr.com',
+        imgUrl: 'assets/images/miscce2.jpg'
+    },
+    {
+        title: 'Al Cuadrado en Detalle',
+        url: 'https://alcuadradoendetalle.tumblr.com',
+        imgUrl: 'assets/images/ce2endetalle.jpg'
+    },
+    {
+        title: 'Cristian Ve Docs',
+        url: 'https://cristianvedocs.tumblr.com',
+        imgUrl: 'assets/images/cristianvedocs.jpg'
+    }
+];
